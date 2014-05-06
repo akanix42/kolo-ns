1a19b1bc2eccfd1ab4457067b05993bb31d06e32
Nathan Reid <nate@nateandmor.com>
2014-05-05 20:56:10 -0500

    Updated nuget packages

---
+++ src/Kolo.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Kolo.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
